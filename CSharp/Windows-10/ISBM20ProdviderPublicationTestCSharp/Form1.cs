44e3eeb5973fb4af1a2b28ea69ca9c827e8b5a26
claire-wong <67246225+claire-wong@users.noreply.github.com>
2023-03-14 11:49:22 -0400

    File Management

---
+++ CSharp/Windows-10/ISBM20ProdviderPublicationTestCSharp/Form1.cs
@@ -0,0 +1,85 @@
+﻿/* Purpose: This is a simple application that acts as an ISBM publication Provider.
+ *          It demonstrates the idea of using an ISBM Client Adapter to post publication 
+ *          to an ISBM Server Adapter. It should be interoperable with any ISBM compatible
+ *          adapters regardless of the actual service bus that delivers the messages.  
+ *          
+ * Author: Claire Wong
+ * Date Created:  2022/08/13
+ * 
+ * (c) 2022
+ * This code is licensed under MIT license
+ * 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ISBM20ClientAdapter;
+using ISBM20ClientAdapter.ResponseType;
+
+
+namespace ISBM20ProviderPublicationTestCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string bodFilePath = AppDomain.CurrentDomain.BaseDirectory + "BODs\\SyncMeasurements.json";
+            textBoxBOD.Text = File.ReadAllText(bodFilePath);
+        }
+
+        private void buttonOpenSession_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adapter method
+            ProviderPublicationService myProviderPublicationService = new ProviderPublicationService();
+            OpenPublicationSessionResponse myOpenPublicationSessionResponse = myProviderPublicationService.OpenPublicationSession(textBoxHostName.Text, textBoxChannelId.Text, textBoxUserName.Text, textBoxPassword.Text);
+            
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myOpenPublicationSessionResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myOpenPublicationSessionResponse.ReasonPhrase;
+            textBoxResponse.Text = myOpenPublicationSessionResponse.ISBMHTTPResponse;
+
+            textBoxSessionId.Text = myOpenPublicationSessionResponse.SessionID;
+        }
+
+        private void buttonCloseSession_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adapter method
+            ProviderPublicationService myProviderPublicationService = new ProviderPublicationService();
+            ClosePublicationSessionResponse myClosePublicationSessionResponse = myProviderPublicationService.ClosePublicationSession(textBoxHostName.Text, textBoxSessionId.Text, textBoxUserName.Text, textBoxPassword.Text);
+
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myClosePublicationSessionResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myClosePublicationSessionResponse.ReasonPhrase;
+            textBoxResponse.Text = myClosePublicationSessionResponse.ISBMHTTPResponse;
+
+        }
+
+            private void buttonPushlish_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adapter method
+            ProviderPublicationService myProviderPublicationService = new ProviderPublicationService();
+            PostPublicationResponse myPostPublicationResponse = myProviderPublicationService.PostPublication(textBoxHostName.Text, textBoxSessionId.Text, textBoxTopic.Text, textBoxBOD.Text, textBoxUserName.Text, textBoxPassword.Text);
+
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myPostPublicationResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myPostPublicationResponse.ReasonPhrase;
+            textBoxResponse.Text = myPostPublicationResponse.ISBMHTTPResponse;
+            
+            textBoxMessageID.Text = myPostPublicationResponse.MessageID;
+
+        }
+    }
+}
---
+++ CSharp/Windows-10/ISBM20ProdviderRequestTestCSharp/Form1.cs
@@ -0,0 +1,117 @@
+﻿/* Purpose: This is a simple application that acts as an ISBM publication Provider.
+ *          It demonstrates the idea of using an ISBM Client Adapter to post publication 
+ *          to an ISBM Server Adapter. It should be interoperable with any ISBM compatible
+ *          adapters regardless of the actual service bus that delivers the messages.  
+ *          
+ * Author: Claire Wong
+ * Date Created:  2022/08/13
+ * 
+ * (c) 2022
+ * This code is licensed under MIT license
+ * 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ISBM20ClientAdapter;
+using ISBM20ClientAdapter.ResponseType;
+
+
+namespace ISBM20ProviderRequestTestCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string bodFilePath = AppDomain.CurrentDomain.BaseDirectory + "BODs\\ShowMeasurements.json";
+            textBoxBODResponse.Text = File.ReadAllText(bodFilePath);
+        }
+
+        private void buttonOpenSession_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adapter method
+            ProviderRequestService myProviderRequestService = new ProviderRequestService();
+            OpenProviderRequestSessionResponse mProviderRequestServiceResponse = myProviderRequestService.OpenProviderRequestSession(textBoxHostName.Text, textBoxChannelId.Text, textBoxTopic.Text, textBoxUserName.Text, textBoxPassword.Text);
+            
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = mProviderRequestServiceResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = mProviderRequestServiceResponse.ReasonPhrase;
+            textBoxResponse.Text = mProviderRequestServiceResponse.ISBMHTTPResponse;
+
+            textBoxSessionId.Text = mProviderRequestServiceResponse.SessionID;
+        }
+
+        private void buttonCloseSession_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adapter method
+            ProviderRequestService myProviderRequestService = new ProviderRequestService();
+            CloseProviderRequestSessionResponse myCloseProviderRequestSessionResponse = myProviderRequestService.CloseProviderRequestSession(textBoxHostName.Text, textBoxSessionId.Text, textBoxUserName.Text, textBoxPassword.Text);
+
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myCloseProviderRequestSessionResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myCloseProviderRequestSessionResponse.ReasonPhrase;
+            textBoxResponse.Text = myCloseProviderRequestSessionResponse.ISBMHTTPResponse;
+
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adaper method
+            ProviderRequestService myProviderRequestService = new ProviderRequestService();
+            ReadRequestResponse myReadRequestResponse = myProviderRequestService.ReadRequest(textBoxHostName.Text, textBoxSessionId.Text, textBoxUserName.Text, textBoxPassword.Text);
+
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myReadRequestResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myReadRequestResponse.ReasonPhrase;
+            textBoxResponse.Text = myReadRequestResponse.ISBMHTTPResponse;
+
+            if (myReadRequestResponse.StatusCode == 200)
+            {
+                textBoxMessageId.Text = myReadRequestResponse.MessageID;
+                textBoxBODRequest.Text = myReadRequestResponse.MessageContent;
+            }
+        }
+
+        private void buttonResponse_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adapter method
+            ProviderRequestService myProviderRequestService = new ProviderRequestService();
+            PostResponseResponse myPostResponseResponse = myProviderRequestService.PostResponse(textBoxHostName.Text, textBoxSessionId.Text, textBoxMessageId.Text, textBoxBODResponse.Text, textBoxUserName.Text, textBoxPassword.Text);
+
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myPostResponseResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myPostResponseResponse.ReasonPhrase;
+            textBoxResponse.Text = myPostResponseResponse.ISBMHTTPResponse;
+
+            textBoxMessageId.Text = myPostResponseResponse.MessageID;
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            //Calling ISBM Adaper method
+            ProviderRequestService myProviderRequestService = new ProviderRequestService();
+            RemoveRequestResponse myRemoveRequestResponse = myProviderRequestService.RemoveRequest(textBoxHostName.Text, textBoxSessionId.Text, textBoxUserName.Text, textBoxPassword.Text);
+
+            //ISBM Adapter Response
+            textBoxStatusCode.Text = myRemoveRequestResponse.StatusCode.ToString();
+            textBoxReasonPhrase.Text = myRemoveRequestResponse.ReasonPhrase;
+            textBoxResponse.Text = myRemoveRequestResponse.ISBMHTTPResponse;
+
+            textBoxBODRequest.Text = "";
+            textBoxMessageId.Text = "";
+        }
+    }
+}
